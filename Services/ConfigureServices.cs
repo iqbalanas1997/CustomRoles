d8ad72b250917d74edbaa0cd4c663135e859e2fc
aaghouri <ahmedworkid@gmail.com>
2021-09-17 19:39:16 +0500

    ServicesConfiguration

--- Services/ConfigureServices.cs
+++ Services/ConfigureServices.cs
@@ -1,18 +1,25 @@
 ﻿using CustomRoles.Data;
 using CustomRoles.Infrastructure;
 using CustomRoles.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomRoles.Services
 {
-    public static class ServicesConfiguration
+    public static class ConfigureCustomDbContext
     {
-        public static void AddCustomServices(this IServiceCollection services)
+        public static void AddCustomServices(this IServiceCollection services, IConfiguration configuration, string ConnectionString)
         {
+            services.AddDbContext<CustomRolesDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(ConnectionString)));
+
+
             services.AddDbContext<CustomRolesDbContext>();
 
             services.AddTransient<IActionRepo, ActionRepo>();
@@ -20,6 +27,7 @@ namespace CustomRoles.Services
             services.AddTransient<IBusinessRolesRepo, BusinessRolesRepo>();
             services.AddTransient<IBusinessUserRepo, BusinessUserRepo>();
             services.AddTransient<IUserControlRepo, UserControlRepo>();
+
 
         }
     }
